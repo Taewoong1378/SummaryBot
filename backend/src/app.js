411b5ada499247b81a061c23ebb61b6a67e206c3
TAEWOONG KANG <ktw2378@korea.ac.kr>
2023-08-06 16:27:10 +0900

    FEAT : express 백엔드 서버 slack webhook 제작 완료

---
+++ backend/src/app.js
@@ -0,0 +1,62 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import fs from 'fs';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import morgan from 'morgan';
+import multer from 'multer';
+import path from 'path';
+import { slackMessage } from './routes/slack.js';
+
+dotenv.config();
+
+const __dirname = path.resolve();
+
+const app = express();
+const prod = process.env.NODE_ENV === 'production';
+
+const transcriptionDir = path.join(__dirname, 'src/python', 'transcription');
+if (!fs.existsSync(transcriptionDir)) {
+  fs.mkdirSync(transcriptionDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, transcriptionDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, 'transcript.txt');
+  },
+});
+
+const upload = multer({ storage });
+
+if (prod) {
+  app.enable('trust proxy');
+  app.use(morgan('combined'));
+  app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(hpp());
+} else {
+  app.use(morgan('dev'));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    })
+  );
+}
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('hello express');
+});
+
+const port = process.env.PORT || 8000;
+
+app.post('/txt', upload.single('file'), slackMessage);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ backend/src/routes/slack.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
+import { PythonShell } from 'python-shell';
+
+const __dirname = path.resolve();
+
+export const slackMessage = (req, res) => {
+  const inputFile = req.file.path;
+  const mode = req.body.mode;
+  const topic = req.body.topic;
+  const outputFile = path.join(
+    __dirname,
+    'src/python',
+    'output',
+    'transcript_summary.txt'
+  );
+
+  const options = {
+    mode: 'text',
+    pythonOptions: ['-u'],
+    scriptPath: path.join(__dirname, 'src/python'),
+    args: [inputFile, mode, topic, outputFile],
+  };
+
+  PythonShell.run('main.py', options).then(async () => {
+    const data = fs.readFileSync(outputFile, 'utf-8');
+    await axios({
+      url: process.env.SLACK_WEBHOOK_URL,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        text: data,
+      },
+    });
+    res.send('success');
+  });
+};
