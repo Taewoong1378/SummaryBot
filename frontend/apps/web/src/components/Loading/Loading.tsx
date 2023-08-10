328c9c36059935fb05c33d8504f96e0e3f9b8044
TAEWOONG KANG <ktw2378@korea.ac.kr>
2023-08-11 00:59:26 +0900

    FIX : Frontend Loading and Home Design

--- frontend/apps/web/src/components/Loading/Loading.tsx
+++ frontend/apps/web/src/components/Loading/Loading.tsx
@@ -2,12 +2,15 @@ import { Portal } from 'design-system';
 import Lottie from 'lottie-react';
 
 import loadingAnimation from '@assets/loading.json';
+import { useWindowSize } from '@hooks';
 
 export const Loading = () => {
+  const { height } = useWindowSize();
+
   return (
     <Portal>
       <div className='flex h-full w-full flex-col items-center'>
-        <Lottie animationData={loadingAnimation} loop={true} style={{ height: 700, width: 700 }} />
+        <Lottie animationData={loadingAnimation} loop={true} style={{ height, width: 300 }} />
       </div>
     </Portal>
   );
